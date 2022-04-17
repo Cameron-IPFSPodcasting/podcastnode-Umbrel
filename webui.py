7bdd84a463d66633c4fb11154e31f0f6fb618113
Cameron <103131615+Cameron-IPFSPodcasting@users.noreply.github.com>
2022-04-17 11:06:58 -0400

    Add files via upload

--- webui.py
+++ webui.py
@@ -22,21 +22,33 @@ if os.path.exists('ipfs/config'):
 
 @route('/')
 def index():
-  htmlsrc = '<html><head><title>IPFS Podcast Node</title><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head>'
-  htmlsrc += '<body style="background-image: url(\'ipfspod.png\'); background-repeat: no-repeat; background-position: 50% 50%; font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; font-size: 14px; margin: 1em;">'
+  htmlsrc = '<html><head>'
+  htmlsrc += '<title>IPFS Podcast Node</title>'
+  htmlsrc += '<meta name="viewport" content="width=device-width, initial-scale=1.0" />'
+  htmlsrc += '<style>'
+  htmlsrc += 'body { background-image: url("ipfspod.png"); background-repeat: no-repeat; background-position: 50% 50%; font-family: "Helvetica Neue",Helvetica,Arial,sans-serif; font-size: 14px; margin: 1em; } '
+  htmlsrc += 'form { margin-top: 0.5em; width: fit-content; } '
+  htmlsrc += 'form label { display: inline-block; width: 150px; text-align: right; } '
+  htmlsrc += 'form input { margin: 4px; } '
+  htmlsrc += 'form button { margin-left: 70%; white-space: nowrap; } '
+  htmlsrc += 'button { padding: 5px; } '
+  htmlsrc += 'pre { border-radius: 20px; background-color: darkcyan; color: white; opacity: 0.6; padding: 10px; overflow: auto; height: 50%; display: flex; flex-direction: column-reverse; box-shadow: 0px 0px 5px 1px black; white-space: break-spaces; } '
+  htmlsrc += '</style>'
+  htmlsrc += '</head>'
+  htmlsrc += '<body>'
   htmlsrc += '<h2>IPFS Podcasting Node</h2>'
 
   if ipfs_id != '':
     htmlsrc += '<label>IPFS ID : </label> <b>' + str(ipfs_id) + '</b><br/>'
 
-  htmlsrc += '<form action="/" method="post" style="margin-top: 0.5em;">'
-  htmlsrc += '<label style="display: inline-block; width: 150px; text-align: right;">Node E-Mail (optional) : </label> <input id="email" name="email" type="email" placeholder="user@example.com" required title="E-mail Address" value="' + email + '" />'
+  htmlsrc += '<form action="/" method="post">'
+  htmlsrc += '<label>Node E-Mail (optional) : </label><input id="email" name="email" type="email" placeholder="user@example.com" required title="E-mail Address" value="' + email + '" />'
   if storageMax != '':
-    htmlsrc += '<br/><label title="IPFS Datastore Limit" style="display: inline-block; width: 150px; text-align: right;">IPFS "StorageMax" : </label> <input  style="width: 50px;" id="storageMax" name="storageMax" type="number" min="1" max="999" pattern="[1-999]" required title="IPFS Datastore Limit" value="' + str(storageMax) + '" /> GB'
+    htmlsrc += '<br/><label title="IPFS Datastore Limit">IPFS "StorageMax" : </label><input  style="width: 50px;" id="storageMax" name="storageMax" type="number" min="1" max="999" pattern="[1-999]" required title="IPFS Datastore Limit" value="' + str(storageMax) + '" />GB'
 
   htmlsrc += '<br/><button>Save Changes</button>'
   htmlsrc += '</form>'
-  htmlsrc += '<hr/><h3>Log Messages</h3><pre style="border-radius: 20px; background-color: darkcyan; color: white; opacity: 0.6; padding: 10px; overflow: auto; height: 50%; display: flex; flex-direction: column-reverse; box-shadow: 0px 0px 5px 1px black; white-space: break-spaces;" >'
+  htmlsrc += '<hr/><h3>Log Messages</h3><pre>'
   with open('ipfspodcastnode.log', 'r') as pcl:
     logtxt = pcl.read()
     htmlsrc += html.escape(logtxt)
